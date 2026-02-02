8bcf0b556b4e7da9e7e02bc4429f393267d74e9c
Joel E Carlson <joel.elmer.carlson@outlook.com>
2026-02-02 16:34:57 +0000

    stock_01

--- tool/stock_ticker.py
+++ tool/stock_ticker.py
@@ -8,13 +8,17 @@ import time
 import yfinance as yf
 import os
 
+__author__  = "Joel E Carlson"
+__credits__ = [ "joel.elmer.carlson@outlook.com" ]
+__email__   = __credits__[0]
+
 TICKERS = ["QQQ", "SPY"] # ETF
 REFRESH_SECONDS = 60     # 1 minute
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
-def get_price(ticker):
+def get_price(ticker: str) -> float:
     data = yf.Ticker(ticker)
     info = data.history(period="1d", interval="1m")
     if info.empty:
